a46c11643929f75e63ed79c8d481850123b2df43
ElVejigante <ernesto.cancel@gmail.com>
2021-05-17 19:25:10 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 // 1. Use the D3 library to read in `samples.json`.
-d3.json('samples.json').then((dataSamples)=>{
-    var dropDown = d3.select('selDataset');
-    console.log(dataSamples);
+function init() {
+    d3.json('samples.json').then((dataSamples)=>{
+    var dropDown = d3.select('#selDataset');
+    //console.log(dataSamples);
+
+    dataSamples.names.forEach(name => {
+        dropDown.append('option').text(name).property('value', name);
+        //console.log(dataSamples.names[0]);
+
+    });
+    optionChanged(dataSamples.names[0]);
 });
+};
+
