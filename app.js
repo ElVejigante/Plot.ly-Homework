aecfed7c63eae1468c704dc46a48f09a9a68f6ca
ElVejigante <ernesto.cancel@gmail.com>
2021-05-17 20:32:04 -0400

    Bar graph success!

--- app.js
+++ app.js
@@ -18,8 +18,6 @@ function optionChanged(values) {
         //console.log(samples);
         var filterSampleID = samples.filter(value => value.id == values);
         //console.log(filterSampleID);
-        var subjectID = filterSampleID[0].id;
-        //console.log(subjectID);
         var result = filterSampleID[0];
         //console.log(result);
         var otuIDs = result.otu_ids;
@@ -29,7 +27,20 @@ function optionChanged(values) {
         var sampleValues = result.sample_values;
         //console.log(sampleValues);
         var yTicks = otuIDs.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
-        console.log(yTicks);
+        //console.log(yTicks);
+        var barTrace = [{
+            x: sampleValues.slice(0, 10).reverse(),
+            y: yTicks,
+            text: otuLabels.slice(0, 10).reverse(),
+            type: 'bar',
+            orientation: 'h',
+        }];
+        var barLayout = {
+            title: "Top 10 OTU's for Individual",
+            xaxis: {title: "Sample Values"},
+            yaxis: {title: "OTU's"},
+        };
+        Plotly.newPlot('bar', barTrace, barLayout);
 
     });
 };
