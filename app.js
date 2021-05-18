8b86d12b42d583027612f4a7fec4a6120da463b4
ElVejigante <ernesto.cancel@gmail.com>
2021-05-17 20:45:05 -0400

    Update app.js

--- app.js
+++ app.js
@@ -16,18 +16,26 @@ function optionChanged(values) {
     d3.json('samples.json').then((dataSamples)=>{
         var samples = dataSamples.samples;
         //console.log(samples);
+
         var filterSampleID = samples.filter(value => value.id == values);
         //console.log(filterSampleID);
+
         var result = filterSampleID[0];
         //console.log(result);
+
         var otuIDs = result.otu_ids;
         //console.log(otuIDs);
+
         var otuLabels = result.otu_labels;
         //console.log(otuLabels);
+
         var sampleValues = result.sample_values;
         //console.log(sampleValues);
+        
         var yTicks = otuIDs.slice(0,10).map(otuID => `OTU ${otuID}`).reverse();
         //console.log(yTicks);
+        
+        //Bar Graph
         var barTrace = [{
             x: sampleValues.slice(0, 10).reverse(),
             y: yTicks,
@@ -42,6 +50,23 @@ function optionChanged(values) {
         };
         Plotly.newPlot('bar', barTrace, barLayout);
 
+        //Bubble Chart
+        var bubbleTrace =[{
+            x: otuIDs,
+            y: sampleValues,
+            mode: 'markers',
+            marker: {
+                size: sampleValues,
+                color: otuIDs, 
+            },
+            text: otuLabels,
+        }];
+        var bubbleLayout = {
+            title: "OTU ID's vs Sample Values for Test Subject",
+            xaxis: {title: "OTU ID"},
+            yaxis: {title: "Sample Values"},
+        };
+        Plotly.newPlot('bubble', bubbleTrace, bubbleLayout);
     });
 };
 
