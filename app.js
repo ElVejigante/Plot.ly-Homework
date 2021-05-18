710d26badb81b24302761961bef4ecbb97f89ad2
ElVejigante <ernesto.cancel@gmail.com>
2021-05-17 21:02:12 -0400

    Update app.js

--- app.js
+++ app.js
@@ -71,10 +71,10 @@ function optionChanged(values) {
         //Demographic Info
         var info = d3.select('#sample-metadata');
         //console.log(info);
-        
+
         info.html('');
         var metadata = dataSamples.metadata;
-        console.log(metadata);
+        //console.log(metadata);
     });
 };
 
